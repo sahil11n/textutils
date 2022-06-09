ce6c6432fc49d4ba3dd5c238cbffbc99de8e7736
Sahil Nerurker <sahil11nerurker@gmail.com>
2022-06-09 19:02:22 +0530

    comments removed

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -47,8 +47,7 @@ export default function Form(props) {
                 <button type="button" disabled={text.length === 0} className="btn btn-primary" onClick={clearbtn}>Clear All</button>
                 <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
                 <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-                {/* <button type="button" className="btn btn-primary" onClick={bold}>Bold All</button> */}
-
+               
                 <p>text summary is : {text.length}  characters      and      {text.split(/\s+/).filter((element) => { return element.length !== 0 }).length}   words</p>
                 <h2>Preview</h2>
                 <p>{text.length > 0 ? text : "Enter something to preview it here"}</p>
